ce3bc352d16d7faae42c33a91a25cb3e6aba68ef
Blomley <89363067+blomleya@users.noreply.github.com>
2022-01-13 19:11:17 +0000

    Add files via upload

---
+++ Denary to Hexadecimal Conversions.py
@@ -0,0 +1,5 @@
+def  dentohex():
+     hexadecimal = input("Please input your denery number to convert to hex : ")
+     hexadecimal = int(hexadecimal)
+     hexadecimal = hex(hexadecimal)[2:]    
+     print(hexadecimal)
